c367adcd1b42155f075b08d268f2eff41162b732
pross1312 <dangvinhtuong12@gmail.com>
2024-08-22 06:33:11 +0700

    Fill snake

--- Plug.cpp
+++ Plug.cpp
@@ -6,29 +6,51 @@ constexpr Color head_color = RED;
 constexpr float velocity = 250.0f;
 constexpr float radius = 10.0f;
 constexpr float distance = 13.0f;
+constexpr float eye_radius = 2.5f;
 constexpr float max_flexibility = 135.0f/180.0f*PI;// 90 degree
-const size_t n_points = 75;
-std::vector<Vector2> points;
+const size_t n_points = 10;
+
+std::vector<Vector2> vertices;
+
+struct Joint {
+    Vector2 pos;
+    float radius;
+};
+
+std::vector<Joint> joints;;
 
 Vector2 distance_constraint(Vector2 center, Vector2 point, float r) {
     return Vector2Add(center, Vector2Scale(Vector2Normalize(Vector2Subtract(point, center)), r));
 }
 
-void init_points() {
+void init() {
     float half_w = GetScreenWidth()/2.0f;
     float half_h = GetScreenHeight()/2.0f;
-    points.clear();
+
+    joints.clear();
     for (size_t i = 0; i < n_points; i++) {
-        points.push_back({.x = half_w - distance*i, .y = half_h});
+        joints.push_back({
+            .pos = {.x = half_w - distance*i, .y = half_h},
+            .radius = radius,
+        });
     }
+    joints[0].radius *= 1.4f;
+    joints[1].radius *= 1.6f;
+    joints[2].radius *= 1.4f;
+    joints[3].radius *= 1.2f;
     SetMousePosition(half_w, half_h);
+
+    vertices.clear();
+    vertices.reserve(n_points);
+
+    HideCursor();
 }
 
 extern "C" {
 void plug_init(int argc, char** argv) {
     (void)argc, (void)argv;
     TraceLog(LOG_INFO, "-------------PLUG_INIT-------------");
-    init_points();
+    init();
 }
 
 void* plug_pre_reload() {
@@ -38,7 +60,7 @@ void* plug_pre_reload() {
 void plug_post_reload(void *state) {
     (void)state;
     TraceLog(LOG_INFO, "-------------PLUG_RELOAD-------------");
-    init_points();
+    init();
 }
 
 void plug_update(float delta) {
@@ -60,31 +82,53 @@ void plug_update(float delta) {
     } else if (IsKeyUp(KEY_A) || IsKeyUp(KEY_D)) {
         vel.x = 0.0f;
     }
-    points[0] = Vector2Add(points[0], vel);
-    points[0] = GetMousePosition();
-    for (size_t i = 1; i < points.size(); i++) {
-        points[i] = distance_constraint(points[i-1], points[i], distance);
-        Vector2 line1 = Vector2Subtract(points[i-1], points[i]);
-        Vector2 line2 = Vector2Subtract(points[i+1], points[i]);
-        float angle = Vector2Angle(line1, line2);
-        if (abs(angle) < max_flexibility) {
-            float rotating_angle = max_flexibility - abs(angle);
-            // Vector2Rotate and Vector2Angle work clockwise
-            // angle < 0 means actual angle > 180 thus need to rotate counter clockwise
-            if (angle < 0) rotating_angle = -rotating_angle;
-            Vector2 rotated = Vector2Rotate(line2, rotating_angle);
-            points[i+1] = Vector2Add(points[i], rotated);
+    // joints[0].pos = Vector2Add(joints[0].pos, vel);
+
+    joints[0].pos = GetMousePosition();
+    vertices.clear();
+    for (size_t i = 1; i < joints.size(); i++) {
+        joints[i].pos = distance_constraint(joints[i-1].pos, joints[i].pos, distance);
+        if (i+1 < joints.size()) {
+            Vector2 line1 = Vector2Subtract(joints[i-1].pos, joints[i].pos);
+            Vector2 line2 = Vector2Subtract(joints[i+1].pos, joints[i].pos);
+            float angle = Vector2Angle(line1, line2);
+            if (abs(angle) < max_flexibility) {
+                float rotating_angle = max_flexibility - abs(angle);
+                // Vector2Rotate and Vector2Angle work clockwise
+                // angle < 0 means actual angle > 180 thus need to rotate counter clockwise
+                if (angle < 0) rotating_angle = -rotating_angle;
+                Vector2 rotated = Vector2Rotate(line2, rotating_angle);
+                joints[i+1].pos = Vector2Add(joints[i].pos, rotated);
+            }
+        }
+        Vector2 direction = Vector2Normalize(Vector2Subtract(joints[i-1].pos, joints[i].pos));
+        Vector2 left_v = Vector2Rotate(direction, -PI/2.0f);
+        Vector2 right_v = Vector2Rotate(direction, PI/2.0f);
+        if (i == 1) { // push head first
+            float r = joints[i-1].radius;
+            const Vector2& pos = joints[i-1].pos;
+            Vector2 quarter_left_v = Vector2Scale(Vector2Rotate(direction, -PI/4.0f), r);
+            Vector2 quarter_right_v = Vector2Scale(Vector2Rotate(direction, PI/4.0f), r);
+            Vector2 head = Vector2Add(pos, Vector2Scale(direction, r));
+            vertices.push_back(head);
+            vertices.push_back(Vector2Add(pos, quarter_left_v));
+            vertices.push_back(Vector2Add(pos, quarter_right_v));
+            vertices.push_back(Vector2Add(pos, Vector2Scale(left_v, r)));
+            vertices.push_back(Vector2Add(pos, Vector2Scale(right_v, r)));
+        }
+        vertices.push_back(Vector2Add(joints[i].pos, Vector2Scale(left_v, joints[i].radius)));
+        vertices.push_back(Vector2Add(joints[i].pos, Vector2Scale(right_v, joints[i].radius)));
+        if (i+1 == joints.size()) {
+            Vector2 tail = Vector2Add(joints[i].pos, Vector2Scale(direction, -radius));
+            vertices.push_back(tail);
         }
     }
-    if (IsKeyPressed(KEY_SPACE)) {
-        Vector2 line1 = Vector2Subtract(points[0], points[1]);
-        Vector2 line2 = Vector2Subtract(points[2], points[1]);
-        float angle = Vector2Angle(line1, line2);
-        TraceLog(LOG_INFO, "%f", angle*180.0f/PI);
-    }
 
-    DrawSplineLinear(points.data(), points.size(), 4.0f, WHITE);
-    DrawCircleV(points[0], radius, head_color);
+    DrawTriangleStrip(vertices.data(), vertices.size(), GRAY);
+    Vector2 eye_offset = Vector2Scale(Vector2Normalize(Vector2Subtract(vertices[3], vertices[4])), 4.5f);
+    DrawCircleV(Vector2Subtract(vertices[3], eye_offset), eye_radius, SKYBLUE);
+    DrawCircleV(Vector2Add(vertices[4], eye_offset), eye_radius, SKYBLUE);
+    // DrawSplineLinear(points.data(), points.size(), 4.0f, WHITE);
     EndDrawing();
 }
 }
